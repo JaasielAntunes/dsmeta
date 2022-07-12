3bb36f2dfb385bedc23887843d9f4d3b6c186aa9
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-07-11 21:54:02 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
   return (
+    <>
     <h1>Hello World!!</h1>
+    <NotificationButton/>
+    </>
   )
 }
 
