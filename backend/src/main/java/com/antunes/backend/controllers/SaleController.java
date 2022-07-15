6a842f6022a86a39e73c23e5c2f347bcc3411441
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-07-14 22:06:53 -0300

    Twilio SMS

--- backend/src/main/java/com/antunes/backend/controllers/SaleController.java
+++ backend/src/main/java/com/antunes/backend/controllers/SaleController.java
@@ -2,13 +2,13 @@ package com.antunes.backend.controllers;
 
 import com.antunes.backend.entities.Sale;
 import com.antunes.backend.services.SaleService;
+import com.antunes.backend.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,11 +17,20 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 
+	@Autowired
+	private SmsService smsService;
+
 	@GetMapping(value = "/find")
 	public Page<Sale> buscarVendas(
 			@RequestParam(value = "minDate", defaultValue = "") String minDate,
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
+	}
+
+	@GetMapping(value = "/{id}/notification")
+	public ResponseEntity<String> enviarSms(@PathVariable Long id) {
+		smsService.sendSms(id);
+		return ResponseEntity.status(HttpStatus.OK).body("Mensagem enviada com sucesso!");
 	}
 }
